873a04d52ddef85a20c3daaae7d99c2959175816
Douglas Ramiro <douglas@cf.coop.br>
2020-09-13 10:37:48 -0300

    adicionado app

---
+++ src/test/java/br/com/tecnisys/rotas/RotasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tecnisys.rotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
